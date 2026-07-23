e579ed24298eba922a87ec371fedd52e058a1c59
22Kratos <tejassinghkapur@gmail.com>
2026-07-23 20:32:46 +0530

    encoder

---
+++ src/utils/encoder.py
@@ -0,0 +1,30 @@
+"""
+what this module is supposed to do:
+URL encode (encode+decode)
+Double URL encode
+Base64 encoding
+HTML entity encoding
+Hex encoding
+
+for url encoding:
+    i will import urllib.parse
+    define a function urlencode():
+        make the function return urllib.parse.quote(the string)
+    define a function urldecode():
+        make the function return urllib.parse.unquote(the string)
+    print the encoded/decoded statement
+    will have to run an if/else loop depending on whether decoder is called or encoder
+    might run a nested function, i will see
+
+for double url encoding:
+    will import urllib.parse()
+    give it a string
+    make it do a first_encode
+    then will make it a second_encode
+    print the second_encode variable
+    do I have to make a double url decoding? I dont know
+
+for Base64 Encoding/decoding:
+    
+
+"""
